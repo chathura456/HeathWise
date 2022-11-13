ddfb877ec256ee6d097b3c3edae6f405b8a85a76
chathura456 <chathuracde@gmail.com>
2022-11-13 22:43:53 +0530

    sql file changed

--- hospital_management.sql
+++ hospital_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 10, 2022 at 07:08 PM
+-- Generation Time: Nov 13, 2022 at 07:22 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -20,6 +20,122 @@ SET time_zone = "+00:00";
 --
 -- Database: `hospital_management`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patientlab`
+--
+
+CREATE TABLE `patientlab` (
+  `SSNo` int(20) NOT NULL,
+  `First_Name` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Reg_Date` varchar(50) NOT NULL,
+  `Appointed_Specialist` varchar(30) NOT NULL,
+  `Appointed_Dr` varchar(30) NOT NULL,
+  `DId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patientlab`
+--
+
+INSERT INTO `patientlab` (`SSNo`, `First_Name`, `Last_Name`, `Reg_Date`, `Appointed_Specialist`, `Appointed_Dr`, `DId`) VALUES
+(20312, 'Smith', 'Fdo', '10/25/2022', 'Urologists', 'David', 42705),
+(20315, 'Julia', 'Smith', '11/10/2022', 'Cardiologists', 'Nimal', 42709),
+(20303, 'Vans', 'Silva', '0000-00-00', 'Cardiologists', 'Nimal', 42709);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patientsdetails`
+--
+
+CREATE TABLE `patientsdetails` (
+  `SSNo` int(20) NOT NULL,
+  `First_Name` varchar(50) NOT NULL,
+  `Last_name` varchar(50) NOT NULL,
+  `Gender` varchar(50) NOT NULL,
+  `Birth_Date` varchar(50) NOT NULL,
+  `Phone_No` varchar(50) NOT NULL,
+  `Weight` varchar(50) DEFAULT NULL,
+  `height` varchar(50) NOT NULL,
+  `Blood_Type` varchar(50) NOT NULL,
+  `Address` varchar(50) NOT NULL,
+  `Reg_Date` varchar(50) NOT NULL,
+  `Appointed_Specialist` varchar(30) NOT NULL,
+  `Appointed_Dr` varchar(30) NOT NULL,
+  `DId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patientsdetails`
+--
+
+INSERT INTO `patientsdetails` (`SSNo`, `First_Name`, `Last_name`, `Gender`, `Birth_Date`, `Phone_No`, `Weight`, `height`, `Blood_Type`, `Address`, `Reg_Date`, `Appointed_Specialist`, `Appointed_Dr`, `DId`) VALUES
+(20309, 'George', 'silva', 'Male', '10/24/2022', '07112344567', '78', '178cm', 'B', 'colombo 10', '10/24/2022', 'Urologists', 'David', 42705),
+(20310, 'Leonard', 'Smith', 'Male', '12/26/1988', '0772341232', '78', '178cm', 'B', 'Colombo 02', '10/25/2022', 'Urologists', 'David', 42705),
+(20311, 'Alan', 'Smith', 'Male', '10/25/2022', '0711256567', '78', '167', 'B', 'Colombo 10', '10/25/2022', 'Urologists', 'David', 42705),
+(20312, 'Smith', 'Fdo', 'Male', '2/2/2000', '07445656332', '78', '178cm', 'B+', 'Clombo 9', '10/25/2022', 'Urologists', 'David', 42705),
+(20313, 'Eric', 'Silva', 'Male', 'Monday, January 9, 1989', '0726786786', '78', '178', 'B', 'Colombo 3', '11/5/2022', 'Cardiologists', 'Nimal', 42709),
+(20314, 'Ajith', 'Perera', 'Male', 'Saturday, November 5, 2022', '0726786783', '56', '156', 'A', 'Kandy', '11/5/2022', 'Urologists', 'David', 42705),
+(20315, 'Julia', 'Smith', 'Female', 'Thursday, November 10, 2022', '0752525684', '72', '185', 'A+', 'Colombo 10', '11/10/2022', 'Cardiologists', 'Nimal', 42709),
+(20316, 'Namal', 'Perera', 'Male', 'Wednesday, November 9, 2022', '07525684', '70', '175', 'O+', 'colombo', '11/16/2022', 'Cardiologists', 'Nimal', 42709);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patientsprescription`
+--
+
+CREATE TABLE `patientsprescription` (
+  `SSNo` int(20) NOT NULL,
+  `First_Name` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Date` varchar(50) DEFAULT NULL,
+  `Age` varchar(30) DEFAULT NULL,
+  `Diagnosis` varchar(150) NOT NULL,
+  `Medicines` varchar(150) NOT NULL,
+  `Notes` varchar(150) DEFAULT NULL,
+  `Appointed_Dr` varchar(30) NOT NULL,
+  `DId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patientsprescription`
+--
+
+INSERT INTO `patientsprescription` (`SSNo`, `First_Name`, `Last_Name`, `Date`, `Age`, `Diagnosis`, `Medicines`, `Notes`, `Appointed_Dr`, `DId`) VALUES
+(20308, 'yiuyi', 'jhkjh', NULL, '323', 'Aids', 'No medicins', 'God bless', 'David', 42705),
+(20314, 'Ajith', 'Perera', NULL, '23', 'fever', 'panadol', '', 'David', 42705),
+(20303, 'Vans', 'Silva', NULL, '25', 'Fever', 'Panadol', '', 'Nimal', 42709);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patientswaiting`
+--
+
+CREATE TABLE `patientswaiting` (
+  `SSNo` int(20) NOT NULL,
+  `First_Name` varchar(50) NOT NULL,
+  `Last_Name` varchar(50) NOT NULL,
+  `Reg_Date` varchar(50) NOT NULL,
+  `Appointed_Specialist` varchar(30) NOT NULL,
+  `Appointed_Dr` varchar(30) NOT NULL,
+  `DId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patientswaiting`
+--
+
+INSERT INTO `patientswaiting` (`SSNo`, `First_Name`, `Last_Name`, `Reg_Date`, `Appointed_Specialist`, `Appointed_Dr`, `DId`) VALUES
+(20304, 'George', 'SIlva', '0000-00-00', 'Endocrinologists', 'Mary', 42712),
+(20313, 'Eric', 'Silva', '11/5/2022', 'Cardiologists', 'Nimal', 42709),
+(20314, 'Ajith', 'Perera', '11/5/2022', 'Urologists', 'David', 42705),
+(20316, 'Namal', 'Perera', '11/16/2022', 'Cardiologists', 'Nimal', 42709);
 
 -- --------------------------------------------------------
 
@@ -66,6 +182,12 @@ INSERT INTO `users` (`Id`, `First_Name`, `Last_name`, `Gender`, `Birth_Date`, `P
 --
 
 --
+-- Indexes for table `patientsdetails`
+--
+ALTER TABLE `patientsdetails`
+  ADD PRIMARY KEY (`SSNo`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -77,10 +199,16 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `patientsdetails`
+--
+ALTER TABLE `patientsdetails`
+  MODIFY `SSNo` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20317;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42719;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42720;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
